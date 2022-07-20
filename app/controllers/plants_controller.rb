610465b1ff8a4d73f711efed51894be95cd0de58
Maggie Saperstein <maggie.saperstein@gmail.com>
2022-07-20 14:48:34 -0500

    commit tree bb18567a32c1db31c4fb1dabfb7bd27688beba72

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -18,9 +18,24 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  
+  def update
+    plant = Plant.find_by(id: params[:id])
+    plant.update(plant_params)
+    render json: plant
+  end
+  
+  def destroy
+    plant = Plant.find_by(id: params[:id])
+    plant.destroy
+    head :no_content
+  end
+  
+  
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
   end
+
 end
